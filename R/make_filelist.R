a194a4633f19a7f5f5ff8a6a51153fddc9c8ca5e
Felipe Campelo <fcampelo@gmail.com>
2022-12-08 14:49:35 +0000

    make file list

---
+++ R/make_filelist.R
@@ -0,0 +1,10 @@
+fn <- dir("../data", pattern = ".rds")
+
+tx <- unname(sapply(fn, function(x) strsplit(x, split = "_")[[1]][1]))
+tx <- gsub("txid", "", tx)
+
+write.csv(data.frame(txid =tx, file = fn),
+          file = "../filelist.csv", 
+          row.names = FALSE, quote = FALSE)
+
+          
