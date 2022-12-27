6e134f091e30c7395663c4835ab02a38e09f1cd4
fcampelo <fcampelo@gmail.com>
2022-12-27 22:53:19 +0000

    Update usage_example.R

--- R/usage_example.R
+++ R/usage_example.R
@@ -6,7 +6,7 @@ epitopes::get_basic_data("../tmp", datasets = c("taxonomy", "proteins",
                                                 "protein_dissimilarity"))
 
 # Add the desired taxonomy IDs (any taxonomic level should work)
-my_txids <- "10242"
+my_txids <- "2732005"
 
 # Retrieve dataset (if multiple ids are provided, returns a list object of the 
 # same length as my_txids)
@@ -15,9 +15,11 @@ X <- get_precomputed_datasets(my_txids,
                               min_peptide = 5, max_epitope = 25)
 
 # Make data splits for, e.g., training and testing.
-
+# If X is a list of multiple taxids, then it need to be run for each dataset.
 X <- make_data_splits(X, 
                       proteins = readRDS("../tmp/proteins.rds"), 
                       target_props = rep(1/3, 3), 
-                      similarity_threshold = .7, 
+                      similarity_threshold = .6, 
+                      id_force_splitting = "10242", # Lowest splitting level (optional)
+                      tax_list = readRDS("../tmp/taxonomy.rds"),
                       diss_matrix = readRDS("../tmp/protein_dissimilarity.rds"))
