67c0d3c6ec1072b98d3f6970e12d1863b31b6d71
fcampelo <fcampelo@gmail.com>
2022-12-27 16:46:34 +0000

    add file list

--- R/make_filelist.R
+++
@@ -1,9 +0,0 @@
-fn <- dir("../data", pattern = ".rds")
-
-tx <- unname(sapply(fn, function(x) strsplit(x, split = "_")[[1]][1]))
-
-write.csv(data.frame(txid =tx, file = fn),
-          file = "../filelist.csv", 
-          row.names = FALSE, quote = FALSE)
-
-          
